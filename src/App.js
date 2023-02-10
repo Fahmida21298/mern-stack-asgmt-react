2d23038083f033c858af5d9c7d0b54dde7386149
Sakib User Fahmi <abdulaziz21298@gmail.comgit config --global user.email abdulaziz21298@gmail.comgit config --global user.email abdulaziz21298@gmail.com>
2023-02-10 20:16:14 +0600

    I've learnt about useState and made a project counter type site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setcounter] = useState(0)
+  // let counter =0
+  const increaseHandler1 =()=>{
+    if(counter>=0){
+
+      setcounter(counter +1)
+    }
+  }
+  const increaseHandler100 =()=>{
+    if(counter>=0){
+
+      setcounter(counter +100)
+    }
+
+  }
+  const decreaseHandler1 =()=>{
+    if(counter>0){
+
+      setcounter(counter -1)
+    }
+
+  }
+  const decreaseHandler100 =()=>{
+    if(counter>=100){
+
+      setcounter(counter -100)
+    }
+
+  }
+  const remove =()=>{
+    setcounter(0)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The value of the counter is {counter} </h1>
+        <div className='calbtn'>
+        <button className='btn btn-info' onClick={increaseHandler1} >Add the counter by 1</button>
+        <button className='btn btn-success' onClick={decreaseHandler1} >subtract the counter by  1</button>
+        <button className='btn btn-info' onClick={increaseHandler100} >Add the counter by 100</button>
+        <button className='btn btn-success' onClick={decreaseHandler100} >subtract the counter by 100</button>
+        </div>
+        <button className='btn btn-danger' onClick={remove} >Remove All</button>
+
       </header>
     </div>
   );
