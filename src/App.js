5d7c77f8c93ec665a8c8dc3d47ccc7bb78a8bba0
Sakib User Fahmi <abdulaziz21298@gmail.comgit config --global user.email abdulaziz21298@gmail.comgit config --global user.email abdulaziz21298@gmail.com>
2023-02-16 13:02:06 +0600

    dynamic counter app

--- src/App.js
+++ src/App.js
@@ -5,48 +5,34 @@ import { useState } from 'react';
 function App() {
 
   const [counter, setcounter] = useState(0)
-  // let counter =0
-  const increaseHandler1 =()=>{
-    if(counter>=0){
 
-      setcounter(counter +1)
-    }
-  }
-  const increaseHandler100 =()=>{
-    if(counter>=0){
-
-      setcounter(counter +100)
-    }
+  const increaseHandler = (a) => {
+      setcounter(counter + a)
 
   }
-  const decreaseHandler1 =()=>{
-    if(counter>0){
-
-      setcounter(counter -1)
-    }
-
+  const decreaseHandler = (b) => {
+      setcounter(counter - b)
   }
-  const decreaseHandler100 =()=>{
-    if(counter>=100){
-
-      setcounter(counter -100)
-    }
-
-  }
-  const remove =()=>{
+  const remove = () => {
     setcounter(0)
+
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1>The value of the counter is {counter} </h1>
         <div className='calbtn'>
-        <button className='btn btn-info' onClick={increaseHandler1} >Add the counter by 1</button>
-        <button className='btn btn-success' onClick={decreaseHandler1} >subtract the counter by  1</button>
-        <button className='btn btn-info' onClick={increaseHandler100} >Add the counter by 100</button>
-        <button className='btn btn-success' onClick={decreaseHandler100} >subtract the counter by 100</button>
+          <button className='btn btn-info' onClick={()=>increaseHandler(1)} >Add by 1</button>
+          <button className='btn btn-success' onClick={()=>decreaseHandler(1)} >subtract by  1</button>
+          <button className='btn btn-info' onClick={()=>increaseHandler(100)} >Add by 100</button>
+          <button className='btn btn-success' onClick={()=>decreaseHandler(100)} >subtract by 100</button>
+          <button className='btn btn-info' onClick={()=>increaseHandler(1000)} >Add by 1000</button>
+         
+          <button className='btn btn-success' onClick={()=>decreaseHandler(1000)} >subtract by 1000</button>
         </div>
         <button className='btn btn-danger' onClick={remove} >Remove All</button>
+
+       
 
       </header>
     </div>
---
+++ src/extra.js
@@ -0,0 +1,19 @@
+  // const [skills, setskills] = useState(['js', 'html', 'css'])
+  // const removeskills = (item)=>{
+  //   const removeskill = skills.filter(skill => skill !== item)
+  //   setskills(removeskill)
+  // }
+  // let counter =0\
+
+   /* <ul>
+          {skills.map(item => (
+            <li>
+              <span>
+              {item} 
+              </span>
+                <button className='btn btn-danger' onClick={()=>removeskills(item)} >Remove me</button>
+
+            </li>
+
+          ))}
+        </ul> */
